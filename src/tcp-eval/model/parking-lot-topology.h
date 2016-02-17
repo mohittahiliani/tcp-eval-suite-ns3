b84eeb33b7220bc306a3969e0c28aaf67c4fef76
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-02-17 19:49:24 +0530

    Parking lot topology header

---
+++ src/tcp-eval/model/parking-lot-topology.h
@@ -0,0 +1,116 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 NITK, Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Dharmendra Kumar Mishra <dharmendra.nitk@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// Define an object to create a parking-lot topology in tcp-eval.
+
+#ifndef PARKING_LOT_TOPOLOGY_H
+#define PARKING_LOT_TOPOLOGY_H
+
+#include <stdint.h>
+#include "ns3/configure-topology.h"
+#include "ns3/point-to-point-parking-lot.h"
+#include "ns3/traffic-parameters.h"
+#include "ns3/create-graph.h"
+#include "ns3/create-traffic.h"
+
+namespace ns3 {
+
+/**
+ * \brief Configures parking-lot topology and simulates the traffic accordingly.
+ *
+ *  It extends ConfigureTopology and invokes PointToPointParkingLotHelper to create a
+ *  the topology. Figure 3 in the following link represents the parking-lot topology
+ *  https://tallercomunicaciones.wikispaces.com/file/view/tcpeval-manual-0.2.pdf
+ */
+class ParkingLotTopology : public ConfigureTopology
+{
+public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+
+  /**
+   * \brief Constructor
+   */
+  ParkingLotTopology (void);
+
+  /**
+   * \brief Destructor
+   */
+  ~ParkingLotTopology (void);
+
+  /**
+   * \brief Converts a value to string.
+   *
+   * This method is used because std::to_string() works with c++11
+   * and std::itoa is not a standard library
+   *
+   * \param data The value which is to be converted to string.
+   */
+  template <typename T>
+  std::string to_string (const T& data);
+
+  /**
+   * \brief Calls a private method CreateParkingLotTopology
+   *
+   * This method is declared virtual in ConfigureTopology class. TCP evaluation topologies like
+   * dumbbell and parking-lot can use this method to configure traffic parameters and invoke the private
+   * methods of their class. 
+   * 
+   * \param traffic Object of TrafficParameters class that contains the 
+   *                information of traffic related parameters.
+   * \param createGraph Object of CreateGraph class that contains the 
+   *                    information of graph related parameters.
+   * \param fileName the name of the file where stats are dumped.
+   */
+  void CreateTopology (Ptr<TrafficParameters> traffic, Ptr<CreateGraph> createGraph, std::string fileName);
+
+  /**
+   * \brief Get delay of cross links in seconds
+   *
+   * \return delay of cross links in seconds
+   */
+  Time GetCrossLinkDelay (void) const;
+
+private:
+  Time     m_crossLinkDelay;      //!< Cross link Delay in ms
+
+  /**
+   * \brief Invokes methods for creating parking-lot topology and simulating traffic
+   *
+   * It configures PointToPointChannel, invokes topology creation class and then calls methods
+   * to create traffic on this topology. Finally, this method invokes Stats class to trace the
+   * required statistics.
+   *
+   * \param traffic Object of TrafficParameters class that contains the 
+   *                information of traffic related parameters.
+   * \param createGraph Object of CreateGraph class that contains the 
+   *                    information of graph related parameters.
+   * \param fileName the name of the file where stats are dumped.
+   */
+  void CreateParkingLotTopology (Ptr<TrafficParameters> traffic, Ptr<CreateGraph> createGraph, std::string fileName);
+};
+
+}
+
+#endif /*PARKING_LOT_TOPOLOGY_H*/
