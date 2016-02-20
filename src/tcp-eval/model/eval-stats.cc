e8ae94b20d600411758c193410b8bf2c3841947e
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-02-20 19:48:17 +0530

    eval stats included

---
+++ src/tcp-eval/model/eval-stats.cc
@@ -0,0 +1,142 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Pratheek B. <pratheekbkasaragod@gmail.com>
+ *          Sourabh S. Shenoy <sourabhsshenoy@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// Implement an object that collects statistics at the bottleneck router.
+
+#include "eval-stats.h"
+
+namespace ns3 {
+
+LinkStats::LinkStats (uint32_t bandwidth, Time rttp, std::string fileName)
+{
+  m_totalUtilization = 0;
+  m_totalQueueSize = 0;
+  m_totalDroppedPacketRate = 0;
+  m_linkStatsFileName.assign (fileName);
+  m_bytesOut = 0;
+  m_sumQueueLength = 0;
+  m_nthSampleInInterval = 0;
+  this->m_bandwidth = bandwidth;
+  this->m_rttp = rttp;
+}
+
+LinkStats::~LinkStats ()
+{
+  InsertIntoFile ();
+  m_linkStatsFile.close ();
+}
+
+// Computes link utilization and mean queue size
+void
+LinkStats::ComputeMetrics ()
+{
+  m_totalUtilization += (double) m_bytesOut * 8.0 / ( m_bandwidth * 1000 * 1000 );
+  m_bytesOut = 0;
+
+  UintegerValue queueSize;
+  if (m_bottleneckQueue.compare ("RED") == 0)
+    {
+      m_queue->GetAttribute ("QueueLimit", queueSize);
+    }
+  else
+    {
+      m_queue->GetAttribute ("MaxPackets", queueSize);
+    }
+
+  m_totalQueueSize += ((m_nthSampleInInterval == 0) ? 0 : ((double) (m_sumQueueLength / m_nthSampleInInterval) / queueSize.Get () * 100));
+  m_sumQueueLength = 0;
+  m_nthSampleInInterval = 0;
+}
+
+// Inserts the values computed in the ComputeMetrics into the file
+void
+LinkStats::InsertIntoFile ()
+{
+  uint32_t totalPackets = m_queue->GetTotalReceivedPackets ();
+  uint32_t droppedPackets = m_queue->GetTotalDroppedPackets ();
+  m_totalDroppedPacketRate = ((droppedPackets == 0) ? 0 : (100.0 * (double) droppedPackets / (double) totalPackets));
+
+  m_linkStatsFile.open (m_linkStatsFileName.c_str (), std::ios::app);
+  m_linkStatsFile << m_bandwidth << std::setw (15) << m_rttp.GetSeconds () << std::setw (15) << m_numFtpFlows;
+  m_linkStatsFile << std::setw (15) << (m_totalUtilization / (m_simulationTime.ToDouble (Time::S)) * 100);
+
+  UintegerValue queueSize;
+  if (m_bottleneckQueue.compare ("RED") == 0)
+    {
+      m_queue->GetAttribute ("QueueLimit", queueSize);
+    }
+  else
+    {
+      m_queue->GetAttribute ("MaxPackets", queueSize);
+    }
+  m_linkStatsFile << std::setw (15) << m_totalQueueSize / (m_simulationTime.ToDouble (Time::S));
+  m_linkStatsFile << std::setw (15) << m_totalDroppedPacketRate;
+
+  m_linkStatsFile << std::endl;
+}
+
+// Called during the PhyTxBegin event at the netdevice.
+// Gets the size of the packet and stores it in bytesOut variable.
+void
+LinkStats::AggregateOverInterval (Ptr<const Packet> packet)
+{
+  m_bytesOut += packet->GetSize ();
+}
+
+// Called during the Enqueue event at the queue.
+// Gets the number of packets in the queue and the number of times it is sampled.
+void
+LinkStats::AggregateQueue (Ptr<const Packet> packet)
+{
+  m_sumQueueLength += m_queue->GetNPackets ();
+  m_nthSampleInInterval++;
+}
+
+
+// It takes node as input and gets references to netdevice and queue in that node.
+// It then connects the trace sources PhyTxBegin and Enqueue to the netdevice and queue respectively
+// and makes callbacks to the methods AggregateOverInterval and AggregateQueue.
+void
+LinkStats::Install (Ptr<Node> node, Ptr<TrafficParameters> traffic)
+{
+  m_simulationTime = traffic->GetSimulationTime ();
+  m_numFtpFlows = traffic->GetNumOfFwdFtpFlows ();
+  if (traffic->IsAqmUsed () == true)
+    {
+      m_bottleneckQueue = "RED";
+    }
+  else
+    {
+      m_bottleneckQueue = "DropTail";
+    }
+  m_netDevice = node->GetDevice (0)->GetObject<PointToPointNetDevice> ();
+  m_queue = m_netDevice->GetQueue ();
+
+  m_netDevice->TraceConnectWithoutContext ("PhyTxBegin", MakeCallback (&LinkStats::AggregateOverInterval, this));
+  m_queue->TraceConnectWithoutContext ("Enqueue", MakeCallback (&LinkStats::AggregateQueue, this));
+
+  for (uint32_t i = 1; i <= m_simulationTime.ToInteger (Time::S); ++i)
+    {
+      Simulator::Schedule (Seconds (i), &LinkStats::ComputeMetrics, this);
+    }
+}
+}
---
+++ src/tcp-eval/model/eval-stats.h
@@ -0,0 +1,138 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Pratheek B. <pratheekbkasaragod@gmail.com>
+ *          Sourabh S. Shenoy <sourabhsshenoy@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// Define an object that collects statistics at the bottleneck router.
+
+#ifndef STATS_TCP_H
+#define STATS_TCP_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/traffic-parameters.h"
+
+#include "ns3/point-to-point-module.h"
+#include "ns3/point-to-point-layout-module.h"
+#include "ns3/applications-module.h"
+
+namespace ns3 {
+
+/**
+ * \brief Calculates the bottleneck metrics and outputs it to files.
+ *
+ *  When installed on a node, it computes metrics such as link utilization,
+ *  mean queue length and packet drop rate.
+ */
+class LinkStats : public Object
+{
+public:
+
+  /**
+   * \brief Constructor
+   *
+   * \param bandwidth Bandwidth used in the current simulation.
+   * \param rttp Round Trip Time used in the current simulation.
+   * \param filename The file to which the computed values are stored.
+   */
+  LinkStats (uint32_t bandwidth, Time rttp, std::string fileName);
+
+  /**
+   * \brief Destructor
+   */
+  ~LinkStats ();
+
+  /**
+   * \brief Calculates metrics every second.
+   *
+   * Computes metrics such as link utilization, queue size every second and
+   * stores the value so the overall utilization and queue size can be computed later
+   */
+  void ComputeMetrics ();
+
+  /**
+   * \brief Computes the number of bytes sent
+   *
+   * It is called everytime a PhyTxBegin event occurs. We calculate the size of the
+   * packet sent and store the value to compute the link utilization at a later stage.
+   *
+   * \param packet Packet whose size is to be obtained
+   */
+  void AggregateOverInterval (Ptr<const Packet> packet);
+
+  /**
+   * \brief Computes the size of the queue
+   *
+   * It is called everytime an Enqueue event occurs. We calculate the number of packets in
+   * the queue and store the value to compute the mean queue size at a later stage.
+   *
+   * \param packet To conform with the signature of the method associated with the callback.
+   *
+   */
+  void AggregateQueue (Ptr<const Packet> packet);
+
+  /**
+   * \brief Writes the metrics into a file.
+   *
+   * Metrics computed in the ComputeMetrics are written into a file.
+   */
+  void InsertIntoFile ();
+
+  /**
+    * \brief Connects the Trace Source of the node passed, to the callback functions
+    *
+    * Obtains references to netdevice and the queue from the node passed to it.
+    * Connects the PhyTxBegin trace source of netdevice to AggregateOverInterval Callback and
+    * Enqueue trace source to the AggregateQueue callback method
+    *
+    * \param node To obtain references to the netdevice and subsequently the queue.
+    * \param traffic To obtain simulation time.
+    *
+    */
+  void Install (Ptr<Node> node, Ptr<TrafficParameters> traffic);
+
+private:
+  uint32_t                    m_bytesOut;		//!< Number of bytes sent per second
+  uint32_t                    m_bandwidth;		//!< Bandwidth of bottleneck link in Mbps
+  uint32_t                    m_sumQueueLength;		//!< Sum of sampled queue lengths
+  uint32_t                    m_nthSampleInInterval;	//!< Number of samples for queue lengths
+  uint32_t                    m_numFtpFlows;		//!< Number of forward FTP Flows
+  double                      m_totalUtilization;	//!< Running sum of all utilization values
+  double                      m_totalQueueSize;		//!< Running sum of queue size
+  double                      m_totalDroppedPacketRate; //!< Running sum of dropped packet rates
+  std::string                 m_bottleneckQueue;	//!< Type of bottleneck Queue
+  Time                        m_simulationTime;		//!< Simulation time in seconds
+  Time                        m_rttp;			//!< RTT value for the simulation
+  Ptr<PointToPointNetDevice>  m_netDevice;		//!< The netdevice from which stats are collected
+  Ptr<Queue>                  m_queue;			//!< The queue of the node from which stats are collected
+  std::string                 m_linkStatsFileName;	//!< Name of file where the output is stored
+  std::ofstream               m_linkStatsFile;		//!< The file for storing the output
+};
+
+}
+
+#endif /*STATS_TCP_H*/
